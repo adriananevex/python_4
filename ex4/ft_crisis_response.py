703dcbd2a441f64bb806a90e2d0adfedf6483a58
Adriana Neves <nevesaera@outlook.com>
2026-04-03 19:55:36 +0100

    4 done done

--- ex4/ft_crisis_response.py
+++ ex4/ft_crisis_response.py
@@ -1,25 +1,24 @@
 def crisis_handler(filename: str) -> None:
-    if filename == "standard_archive.txt":
-        print(f"ROUTINE ACCESS: Attempting access to '{filename}'")
-    else:
-        print(f"CRISIS ALERT: Attempting access to '{filename}'")
-
     try:
         with open(filename, "r", encoding="utf-8") as file:
             data = file.read()
 
-        print(f"SUCCESS: Archive recovered - {data}")
+        print(f"ROUTINE ACCESS: Attempting access to '{filename}'...")
+        print(f"SUCCESS: Archive recovered - ''{data}''")
         print("STATUS: Normal operations resumed")
 
     except FileNotFoundError:
+        print(f"CRISIS ALERT: Attempting access to '{filename}'...")
         print("RESPONSE: Archive not found in storage matrix")
         print("STATUS: Crisis handled, system stable")
 
     except PermissionError:
+        print(f"CRISIS ALERT: Attempting access to '{filename}'...")
         print("RESPONSE: Security protocols deny access")
         print("STATUS: Crisis handled, security maintained")
 
     except Exception:
+        print(f"CRISIS ALERT: Attempting access to '{filename}'...")
         print("RESPONSE: Unexpected system anomaly encountered")
         print("STATUS: Crisis contained, diagnostic required")
 
